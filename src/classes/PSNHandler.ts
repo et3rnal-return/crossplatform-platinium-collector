12a23889460afea855e1a2a9db384cbefb16ccab
Konrad Ambroży <konrad.ambrozy9@gmail.com>
2022-04-24 15:30:43 +0200

    basic classes

---
+++ src/classes/PSNHandler.ts
@@ -0,0 +1,8 @@
+import { PlatformHandler } from "@/classes/PlatformHandler";
+import { PlatiniumTrophy } from "@/types/types";
+
+export class PSNHandler extends PlatformHandler {
+  getTrophies(): PlatiniumTrophy[] {
+    return [];
+  }
+}
